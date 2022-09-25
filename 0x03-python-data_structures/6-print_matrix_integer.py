c5b7291e2644a464c7ec2af7152737a2347fd9ee
ishimwe_chriss <christianishimwe72@gmail.com>
2022-09-25 06:10:16 -0700

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,10 +6,3 @@ def print_matrix_integer(matrix=[[]]):
                 print(" ", end='')
             print("{:d}".format(matrix[i][j]), end='')
         print()
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-S
