a3a2ee96701b80af4393320ff1547db28e37cda8
ishimwe_chriss <christianishimwe72@gmail.com>
2022-10-11 11:55:00 -0700

    Update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# 0-square.py 
-"""Defines a square """
-
-
 class Square:
-    """Does nothing"""
+    """Represents an empty square."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 0-square.py 
+# 0-square.py
 """Defines a square """
 
 
@@ -12,3 +12,5 @@ class Square:
         """
 
         self.__size = size
+~
+
